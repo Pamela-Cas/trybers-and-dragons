321fc339c0168fe1484f7c15e24e145ae00bc605
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-22 16:42:07 -0300

    criando super

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -3,4 +3,10 @@ import Race from './Race';
 class Halfling extends Race {
   private _maxLifePoints: number;
   private static _instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+   
+  }
+
 }

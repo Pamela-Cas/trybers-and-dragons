edf2715e7b4b4db18f588c87a64002c4e2f7619b
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-22 16:30:57 -0300

    criando get maxLifePoints

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,16 +1,18 @@
 import Race from './Race';
 
 class Elf extends Race {
-    private _maxLifePoints: number;
-    private static _instances = 0; 
+  private _maxLifePoints: number;
+  private static _instances = 0; 
 
-    constructor(name: string, dexterity: number) {
-        super(name, dexterity);
-        this._maxLifePoints = 99;
-        Elf._instances += 1;
-    }
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._instances += 1;
+  }
 
-
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
 }
 
 export default Elf;

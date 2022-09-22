16837283fa0750fd2d5bf6b5bccdc5041c8a3873
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-22 17:43:05 -0300

    criando constructor

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -2,6 +2,12 @@ abstract class Archetype {
   private _name: string;
   private _special: number;
   private _cost: number;
+
+  constructor(name: string, special: number, cost: number) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
 }
 
 export default Archetype; 

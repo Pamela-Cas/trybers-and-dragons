c860a9bb2df192210966c9f248bf4b90753f9b2f
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-22 18:19:59 -0300

    criando o constructor

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,7 +1,14 @@
 import Archetype from './Archetype';
-import {  EnergyType } from '../Energy';
+import { EnergyType } from '../Energy';
 
 class Mage extends Archetype {
+  static _instances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';      
+  }
 }
 
 export default Mage;

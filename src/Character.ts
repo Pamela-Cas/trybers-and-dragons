dd73f16eecef07c627c150c0c73256605cd2975c
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-23 15:23:41 -0300

    criando atributo privado LifePoints

--- src/Character.ts
+++ src/Character.ts
@@ -1,8 +1,12 @@
 import Fighter from './Fighter/Fighter';
+import Race from './Races';
 
 class Character implements Fighter {
-    
-
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: MaxLifePoints;
+  private _lifePoints: LifePoints;
+  
 }
 
 export default Character;

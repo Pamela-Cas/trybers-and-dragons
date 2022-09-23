e56f4a14f63ae1907c8a882613103dacc43f82e0
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-23 19:33:27 -0300

    req 08 finalizado

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,18 +1,22 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-interface Fighter {
-  lifePoints: number;
-  strength: number;
+export default interface Fighter extends SimpleFighter{
+  // lifePoints: number;
+  // strength: number;
   defense: number;
-  attack(enemy:Fighter):void;
+  // attack(enemy:Fighter):void;
   levelUp():void;
-  receiveDamage(attackPoints:number):number;
+  // receiveDamage(attackPoints:number):number;
   energy?: Energy;
   special?(enemy: Fighter): void;
 }
 
-export default Fighter;
-
 // pesquisa interface e propriedades opcionais
 // https://bognarjunior.wordpress.com/tag/typescript/
 // https://www.youtube.com/watch?v=TYLvtyKNMao
+
+// refatoração do código conf req 8; 
+// interface SimpleFighter possiu atributos: 
+// lifePoints, strength, attack e receveiDamage;
+// portanto foi extendida em Fighter e retirado do Fighter
--- src/Fighter/SimpleFighter.ts
+++ src/Fighter/SimpleFighter.ts
@@ -1,3 +1,8 @@
 interface SimpleFighter {
-    
+  lifePoints: number;
+  strength: number;
+  attack(enemy:SimpleFighter):void;
+  receiveDamage(receiveDamage:number):number;
 }
+
+export default SimpleFighter;

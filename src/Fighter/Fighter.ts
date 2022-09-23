1d723c0426e8ddc4f6d8326c241ad193de875c42
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-23 13:11:28 -0300

    colocando propriedades opcionais por último

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,11 +4,11 @@ interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
-  energy?: Energy;
   attack(enemy:Fighter):void;
-  special?(enemy: Fighter): void;
   levelUp():void;
   receiveDamage(attackPoints:number):number;
+  energy?: Energy;
+  special?(enemy: Fighter): void;
 }
 
 export default Fighter;

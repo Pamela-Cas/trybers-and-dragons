bee1c243ee5d2c73ed355ccf879229bbbf23e299
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-23 13:00:38 -0300

    criando a interface Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,7 +4,11 @@ interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
-  energy: Energy;
+  energy?: Energy;
+  attack(enemy:Fighter):void;
+  special?(enemy: Fighter): void;
+  levelUp():void;
+  receiveDamage(attackPoints:number):number;
 }
 
 export default Fighter;

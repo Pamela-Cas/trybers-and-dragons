aac92f7658c719faa5b8d80283780a8e30e33abe
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-09-23 17:08:28 -0300

    criando o constructor

--- src/Character.ts
+++ src/Character.ts
@@ -2,19 +2,33 @@ import Archetype from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter/Fighter';
 import Race from './Races';
+import Elf from './Races/Elf';
+import Mage from './Archetypes/Mage';
+import getRandomInt from './utils';
 
 class Character implements Fighter {
   private _race: Race;
   private _archetype: Archetype;
-  private _maxLifePoints: MaxLifePoints;
-  private _lifePoints: LifePoints;
-  private _strength: Strength;
-  private _defense: Defense;
-  private _dexterity: Dexterity;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
   private _energy: Energy;
 
   constructor(name:string) {
-  };
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
 }
 
 export default Character;
